36b6a060330536f194813ab31164f756abc028a3
Steven Woodward <steveanthony999@gmail.com>
2022-07-01 10:46:40 -0700

    styled and laid out menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,6 +8,12 @@ import PestoPizza from '../../assets/pizzapesto.png';
 import WhiteGarlicPizza from '../../assets/pizzagarlicwhite.png';
 import MeatLoversPizza from '../../assets/pizzameatlovers.png';
 import SixCheesePizza from '../../assets/pizzasixcheese.png';
+import Tiramisu from '../../assets/tiramisu.jpg';
+import Waffle from '../../assets/waffle.jpg';
+import Canoli from '../../assets/canoli.jpg';
+import PannaCotta from '../../assets/pannacotta.jpg';
+import Soda from '../../assets/soda.png';
+import Beer from '../../assets/beer.png';
 import {
   GiPlantSeed,
   GiFullPizza,
@@ -22,7 +28,7 @@ const Menu = () => {
     <div id='menu' className='Menu radius mb-4'>
       <h2 className='med-heading'>Our Menu, Always Fresh</h2>
       <div className='rule mt-2'></div>
-      <div className='Menu-section mt-2'>
+      <div className='Menu-section mt-4'>
         <div className='Menu-section-header'>
           <GiPlantSeed />
           <h3 className='sm-heading ml-1'>Salads</h3>
@@ -30,67 +36,372 @@ const Menu = () => {
         <div className='rule mt-1'></div>
         <div className='Menu-items-container mt-2'>
           <div className='Menu-item'>
-            <img src={HouseSalad} alt='house salad' className='Menu-image' />
-            <div className='Menu-item-info ml-2'>
+            <div className='Menu-item-left'>
+              <img src={HouseSalad} alt='house salad' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
               <h4 className='sm-heading'>House Salad</h4>
-              <p className='mt-1'>
+              <p className='Menu-item-description text-slate-grey mt-1'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
+              <div className='Menu-pricing  mt-1'>
+                <p className='Menu-item-description text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
             </div>
           </div>
           <div className='Menu-item'>
-            <img src={GreekSalad} alt='greek salad' className='Menu-image' />
-            <div className='Menu-item-info ml-2'>
+            <div className='Menu-item-left'>
+              <img src={GreekSalad} alt='greek salad' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
               <h4 className='sm-heading'>Greek Salad</h4>
-              <p className='mt-1'>
+              <p className='Menu-item-description text-slate-grey mt-1'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
             </div>
           </div>
           <div className='Menu-item'>
-            <img
-              src={HarvestSalad}
-              alt='harvest salad'
-              className='Menu-image'
-            />
-            <div className='Menu-item-info ml-2'>
+            <div className='Menu-item-left'>
+              <img
+                src={HarvestSalad}
+                alt='harvest salad'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
               <h4 className='sm-heading'>Harvest Salad</h4>
-              <p className='mt-1'>
+              <p className='Menu-item-description text-slate-grey mt-1'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
             </div>
           </div>
           <div className='Menu-item'>
-            <img
-              src={CapreseSalad}
-              alt='caprese salad'
-              className='Menu-image'
-            />
-            <div className='Menu-item-info ml-2'>
+            <div className='Menu-item-left'>
+              <img
+                src={CapreseSalad}
+                alt='caprese salad'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
               <h4 className='sm-heading'>Caprese Salad</h4>
-              <p className='mt-1'>
+              <p className='Menu-item-description text-slate-grey mt-1'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='Menu-section mt-2'>
+      {/* PIZZAS */}
+      <div className='Menu-section mt-4'>
         <div className='Menu-section-header'>
           <GiFullPizza />
           <h3 className='sm-heading ml-1'>Pizzas</h3>
         </div>
+        <div className='rule mt-1'></div>
+        <div className='Menu-items-container mt-2'>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img
+                src={TraditionalPizza}
+                alt='traditional pizza'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Traditional Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing  mt-1'>
+                <p className='Menu-item-description text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img
+                src={MargheritaPizza}
+                alt='margherita pizza'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Margherita Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={PestoPizza} alt='pesto pizza' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Pesto Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img
+                src={WhiteGarlicPizza}
+                alt='white garlic pizza'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>White Garlic Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img
+                src={MeatLoversPizza}
+                alt='meat lovers pizza'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Meat Lovers Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img
+                src={SixCheesePizza}
+                alt='six cheese pizza'
+                className='Menu-image'
+              />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Six Cheese Pizza</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$19.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='BYO-button mt-4'>
+          <button className='button button-white-smoke'>
+            or Build Your Own
+          </button>
+        </div>
       </div>
-      <div className='Menu-section mt-2'>
+      {/* DESSERTS */}
+      <div className='Menu-section mt-4'>
         <div className='Menu-section-header'>
           <GiCakeSlice />
           <h3 className='sm-heading ml-1'>Desserts</h3>
         </div>
+        <div className='rule mt-1'></div>
+        <div className='Menu-items-container mt-2'>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={Tiramisu} alt='tiramisu' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Tiramisu</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing  mt-1'>
+                <p className='Menu-item-description text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={Waffle} alt='waffle' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Waffle and Berries</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={PannaCotta} alt='pannacotta' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Panna Cotta</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={Canoli} alt='canoli' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Canoli</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$9.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='Menu-section mt-2'>
+      {/* DRINKS */}
+      <div className='Menu-section mt-4'>
         <div className='Menu-section-header'>
           <GiSodaCan />
           <h3 className='sm-heading ml-1'>Drinks</h3>
+        </div>
+        <div className='rule mt-1'></div>
+        <div className='Menu-items-container mt-2'>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={Soda} alt='soda' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Soda</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing  mt-1'>
+                <p className='Menu-item-description text-slate-grey'>$2.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='Menu-item'>
+            <div className='Menu-item-left'>
+              <img src={Beer} alt='beer' className='Menu-image' />
+            </div>
+            <div className='Menu-item-right ml-2'>
+              <h4 className='sm-heading'>Beer</h4>
+              <p className='Menu-item-description text-slate-grey mt-1'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div className='Menu-pricing mt-1'>
+                <p className='text-slate-grey'>$4.95</p>
+                <div className='counter'>
+                  <div>-</div>
+                  <div>0</div>
+                  <div>+</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

d56ddc5aba75e02cf84ce51a8c9cf52465209e75
Steven Woodward <steveanthony999@gmail.com>
2022-07-01 22:34:38 -0700

    testing github secrets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Parallax } from 'react-parallax';
 
-import FirebaseAuthService from './FirebaseAuthService';
+// import FirebaseAuthService from './FirebaseAuthService';
 
 import Home from './pages/Home/Home';
 import Admin from './pages/Admin/Admin';
@@ -10,15 +10,13 @@ import Admin from './pages/Admin/Admin';
 import BgPhoto from './assets/pizzasign.jpg';
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  FirebaseAuthService.subscribeToAuthChanges(setUser);
+  // FirebaseAuthService.subscribeToAuthChanges(setUser);
 
-  useEffect(() => {
-    if (user) {
-      console.log(user);
-    }
-  }, [user]);
+  // useEffect(() => {
+  //   console.log(user);
+  // }, [user]);
 
   return (
     <BrowserRouter>

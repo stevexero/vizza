838cbd37be457dea206b232aa6ea3276f2b65bca
Steven Woodward <steveanthony999@gmail.com>
2022-07-01 21:37:24 -0700

    testing key

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,7 +4,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: 'AIzaSyB8hqiMW--LU82xLILus_amLWqMeAeiwpY',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,

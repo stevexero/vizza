b80c4d9d5ea9253cf29d8189357401524a04069f
Steven Woodward <steveanthony999@gmail.com>
2022-07-02 23:08:02 -0700

    added firebase key direct

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -6,7 +6,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  //   apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: 'AIzaSyB8hqiMW--LU82xLILus_amLWqMeAeiwpY',
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,

94efe220950a56c88f7a4db62b519dd94c0db798
Steven Woodward <steveanthony999@gmail.com>
2022-07-11 22:29:21 -0700

    added firebase env vars back

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -10,12 +10,18 @@ import { getStorage } from 'firebase/storage';
 const config = {
   //   apiKey: process.env.REACT_APP_API_KEY,
   apiKey: 'AIzaSyB8hqiMW--LU82xLILus_amLWqMeAeiwpY',
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  authDomain: 'vizza-b0914.firebaseapp.com',
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: 'vizza-b0914',
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  storageBucket: 'vizza-b0914.appspot.com',
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: '107336587806',
+  // appId: process.env.REACT_APP_APP_ID,
+  appId: '1:107336587806:web:5e2e242ad4310226346c0f',
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  measurementId: 'G-0ENFK3FGKZ',
 };
 
 const firebaseApp = initializeApp(config);

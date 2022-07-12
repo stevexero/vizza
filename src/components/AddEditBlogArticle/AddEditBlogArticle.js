9ef7fb1c274cff2794f09705007e9014d6a84fb8
Steven Woodward <steveanthony999@gmail.com>
2022-07-11 22:11:08 -0700

    removed editor

--- src/components/AddEditBlogArticle/AddEditBlogArticle.js
+++ src/components/AddEditBlogArticle/AddEditBlogArticle.js
@@ -1,34 +1,6 @@
-import { useState, useEffect } from 'react';
-import EditorJS from '@editorjs/editorjs';
-
-import Configuration from './editorConfig';
-
 import './AddEditBlogArticle.css';
 
 const AddEditBlogArticle = () => {
-  const [editor, seteditor] = useState({});
-
-  useEffect(() => {
-    const editor = new EditorJS(Configuration());
-    seteditor(editor);
-  }, []);
-
-  const onSave = () => {
-    editor
-      .save()
-      .then((outputData) => {
-        console.log('Article data: ', outputData);
-      })
-      .catch((error) => {
-        console.log('Saving failed: ', error);
-      });
-  };
-
-  return (
-    <div className='AddEditBlogArticle'>
-      <button onClick={onSave}>Save</button>
-      <div id='editorjs'></div>
-    </div>
-  );
+  return <div className='AddEditBlogArticle'></div>;
 };
 export default AddEditBlogArticle;
